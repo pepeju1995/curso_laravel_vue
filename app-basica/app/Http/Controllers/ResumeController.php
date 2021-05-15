9f9fd75340424f94a5c4a54d058e6f6f840c7e47
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-05-15 07:15:04 +0200

    primero pasos laravel

---
+++ app-basica/app/Http/Controllers/ResumeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resume;
+use Illuminate\Http\Request;
+
+class ResumeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data = 'Jose Juan Perez Gonzalez';
+        return view('resumes.create', ['data' => $data]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response("Form received");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Resume $resume)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Resume  $resume
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Resume $resume)
+    {
+        //
+    }
+}
--- app-basica/routes/web.php
+++ app-basica/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/resumes/create', [ResumeController::class, 'create']);
+Route::post('/resumes', [ResumeController::class, 'store'])->name('resumes.store');
